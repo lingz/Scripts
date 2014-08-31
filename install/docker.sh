9f0fff7461627677ef9c49c56906c4624ead6ee9
Lingliang Zhang <lingliangz@gmail.com>
2014-08-30 22:50:17 -0400

    Added docker installer

---
+++ install/docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sh -c "echo deb https://get.docker.io/ubuntu docker main\
+  > /etc/apt/sources.list.d/docker.list"
+apt-get update
+apt-get install lxc-docker
---
+++ install/wicd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt-get install wicd
+apt-get install wicd-curses
