b5f9356cb56d787f1f7263b1130830d32b08ec2c
Lingliang Zhang <lingliangz@gmail.com>
2016-03-07 22:17:28 +0400

    added rc.local

--- mac_keyboard.sh
+++ mac_keyboard.sh
@@ -3,3 +3,5 @@
 echo "1" > /sys/module/hid\_apple/parameters/swap\_opt\_cmd
 # enables tilde
 echo 0 > /sys/module/hid_apple/parameters/iso_layout
+# enables fn keys
+echo "0" > /sys/module/hid_apple/parameters/fnmode
